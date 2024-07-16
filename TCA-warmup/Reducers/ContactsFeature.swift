eec03353ff1abd6610604eacbcec2057aa8b4a1e
jwilson <jwilsonrae@gmail.com>
2024-07-16 17:37:04 +0200

    Multiple presentations

--- TCA-warmup/Reducers/ContactsFeature.swift
+++ TCA-warmup/Reducers/ContactsFeature.swift
@@ -20,12 +20,19 @@ struct ContactsFeature {
     @ObservableState
     struct State: Equatable {
         @Presents var addContact: AddContactFeature.State?
+        @Presents var alert: AlertState<Action.Alert>?
         var contacts: IdentifiedArrayOf<Contact> = []
     }
     enum Action {
         case addButtonTapped
         case addContact(PresentationAction<AddContactFeature.Action>)
-        
+        case deleteButtonTapped(id: Contact.ID)
+        case alert(PresentationAction<Alert>)
+
+        enum Alert: Equatable {
+            case confirmDeletion(id: Contact.ID)
+        }
+
     }
     var body: some ReducerOf<Self> {
         Reduce { state, action in
@@ -46,6 +53,23 @@ struct ContactsFeature {
 //                state.addContact = nil
                 return .none
 
+            case let .alert(.presented(.confirmDeletion(id: id))):
+                state.contacts.remove(id: id)
+                return .none
+
+            case .alert:
+                return .none
+
+            case let .deleteButtonTapped(id: id):
+                state.alert = AlertState {
+                    TextState("Are you sure?")
+                } actions: {
+                    ButtonState(role: .destructive, action: .confirmDeletion(id: id)) {
+                        TextState("Delete")
+                    }
+                }
+                return .none
+
             case .addContact:
                 return .none
             }
@@ -53,5 +77,6 @@ struct ContactsFeature {
         .ifLet(\.$addContact, action: \.addContact) {
             AddContactFeature()
         }
+        .ifLet(\.$alert, action: \.alert)
     }
 }
--- TCA-warmup/Views/ContactsView.swift
+++ TCA-warmup/Views/ContactsView.swift
@@ -10,45 +10,55 @@ import SwiftUI
 
 
 struct ContactsView: View {
-  @Bindable var store: StoreOf<ContactsFeature>
+    @Bindable var store: StoreOf<ContactsFeature>
 
-  var body: some View {
-    NavigationStack {
-      List {
-        ForEach(store.contacts) { contact in
-          Text(contact.name)
-        }
-      }
-      .navigationTitle("Contacts")
-      .toolbar {
-        ToolbarItem {
-          Button {
-            store.send(.addButtonTapped)
-          } label: {
-            Image(systemName: "plus")
-          }
-        }
-      }
-    }
-    .sheet(item: $store.scope(state: \.addContact, action: \.addContact)) { addContactStore in
+    var body: some View {
         NavigationStack {
-            AddContactView(store: addContactStore)
+            List {
+                ForEach(store.contacts) { contact in
+                    HStack {
+                        Text(contact.name)
+                        Spacer()
+                        Button {
+                            store.send(.deleteButtonTapped(id: contact.id))
+                        } label: {
+                            Image(systemName: "trash")
+                                .foregroundColor(.red)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Contacts")
+            .toolbar {
+                ToolbarItem {
+                    Button {
+                        store.send(.addButtonTapped)
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
         }
+        .sheet(item: $store.scope(state: \.addContact, action: \.addContact)) { addContactStore in
+            NavigationStack {
+                AddContactView(store: addContactStore)
+            }
+        }
+        .alert($store.scope(state: \.alert, action: \.alert))
     }
-  }
 }
 #Preview {
-  ContactsView(
-    store: Store(
-      initialState: ContactsFeature.State(
-        contacts: [
-          Contact(id: UUID(), name: "Blob"),
-          Contact(id: UUID(), name: "Blob Jr"),
-          Contact(id: UUID(), name: "Blob Sr"),
-        ]
-      )
-    ) {
-      ContactsFeature()
-    }
-  )
+    ContactsView(
+        store: Store(
+            initialState: ContactsFeature.State(
+                contacts: [
+                    Contact(id: UUID(), name: "Blob"),
+                    Contact(id: UUID(), name: "Blob Jr"),
+                    Contact(id: UUID(), name: "Blob Sr"),
+                ]
+            )
+        ) {
+            ContactsFeature()
+        }
+    )
 }
