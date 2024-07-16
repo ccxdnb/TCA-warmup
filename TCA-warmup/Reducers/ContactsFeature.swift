8dea6c301e786fdc5c68264ddb5f74aec611edd8
jwilson <jwilsonrae@gmail.com>
2024-07-16 18:07:49 +0200

    Improved domain modeling and prevented multiple presentation bug

--- TCA-warmup/Reducers/ContactsFeature.swift
+++ TCA-warmup/Reducers/ContactsFeature.swift
@@ -19,15 +19,13 @@ struct Contact: Equatable, Identifiable {
 struct ContactsFeature {
     @ObservableState
     struct State: Equatable {
-        @Presents var addContact: AddContactFeature.State?
-        @Presents var alert: AlertState<Action.Alert>?
+        @Presents var destination: Destination.State?
         var contacts: IdentifiedArrayOf<Contact> = []
     }
     enum Action {
         case addButtonTapped
-        case addContact(PresentationAction<AddContactFeature.Action>)
         case deleteButtonTapped(id: Contact.ID)
-        case alert(PresentationAction<Alert>)
+        case destination(PresentationAction<Destination.Action>)
 
         enum Alert: Equatable {
             case confirmDeletion(id: Contact.ID)
@@ -38,45 +36,46 @@ struct ContactsFeature {
         Reduce { state, action in
             switch action {
             case .addButtonTapped:
-                state.addContact = AddContactFeature.State(
-                    contact: .init(id: .init(), name: "")
+                state.destination = .addContact(
+                    AddContactFeature.State(
+                        contact: .init(id: .init(), name: "")
+                    )
                 )
                 return .none
 
-//            case .addContact(.presented(.delegate(.cancel))):
-//                state.addContact = nil
-//                return .none
-
-            case let .addContact(.presented(.delegate(.saveContact(contact)))):
-//                guard let contact = state.addContact?.contact else { return .none }
+            case let .destination(.presented(.addContact(.delegate(.saveContact(contact))))):
                 state.contacts.append(contact)
-//                state.addContact = nil
                 return .none
 
-            case let .alert(.presented(.confirmDeletion(id: id))):
+            case let .destination(.presented(.alert(.confirmDeletion(id: id)))):
                 state.contacts.remove(id: id)
                 return .none
 
-            case .alert:
+            case .destination:
+                state.destination = .addContact(.init(contact: .init(id: .init(), name: "")))
                 return .none
 
             case let .deleteButtonTapped(id: id):
-                state.alert = AlertState {
-                    TextState("Are you sure?")
-                } actions: {
-                    ButtonState(role: .destructive, action: .confirmDeletion(id: id)) {
-                        TextState("Delete")
+                state.destination = .alert(
+                    AlertState {
+                        TextState("Are you sure?")
+                    } actions: {
+                        ButtonState(role: .destructive, action: .confirmDeletion(id: id)) {
+                            TextState("Delete")
+                        }
                     }
-                }
-                return .none
-
-            case .addContact:
+                )
                 return .none
             }
         }
-        .ifLet(\.$addContact, action: \.addContact) {
-            AddContactFeature()
-        }
-        .ifLet(\.$alert, action: \.alert)
+        .ifLet(\.$destination, action: \.destination)
+    }
+}
+
+extension ContactsFeature {
+    @Reducer(state: .equatable)
+    enum Destination { // some kind of coordinator
+        case addContact(AddContactFeature)
+        case alert(AlertState<ContactsFeature.Action.Alert>)
     }
 }
